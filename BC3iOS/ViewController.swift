0800c75e79d49c69abe991e19b6f580e7698fd19
weststar25 <jiwoo932@naver.com>
2018-12-13 17:09:51 +0900

    [ADD] .gitignore 추가

---
+++ BC3iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BC3iOS
+//
+//  Created by 김지우 on 2018. 12. 13..
+//  Copyright © 2018년 havetherain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
